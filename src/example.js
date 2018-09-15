4f2fde56e2e63c3f6b3b1fd1cbfe01946c1af08d
Kent C. Dodds <kent@doddsfamily.us>
2018-09-14 20:29:44 -0600

    config prettier

--- src/example.js
+++ src/example.js
@@ -1,11 +1,11 @@
-const name = "Freddy";
-typeof name === "string";
+const name = 'Freddy'
+typeof name === 'string'
 
-if (!("serviceWorker" in navigator)) {
+if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
-const greeting = "hello";
-console.log(`${greeting} world!`);
+const greeting = 'hello'
+console.log(`${greeting} world!`)
 
-[(1, 2, 3)].forEach(x => console.log(x));
+;[(1, 2, 3)].forEach(x => console.log(x))
