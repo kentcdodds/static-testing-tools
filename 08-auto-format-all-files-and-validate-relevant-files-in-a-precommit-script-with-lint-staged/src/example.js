bd158407d093e979e0de953407bf178e062f005c
Zac Jones <zacjones93@gmail.com>
2018-10-24 10:05:44 -0400

    updated to folders in master

---
+++ 08-auto-format-all-files-and-validate-relevant-files-in-a-precommit-script-with-lint-staged/src/example.js
@@ -0,0 +1,10 @@
+const name = 'Freddy'
+typeof name === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+;[(1, 2, 3)].forEach(x => console.log(x))
