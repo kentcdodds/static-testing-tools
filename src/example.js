fbb81df6c9416b2bfcd191fd93ff4fc9b4bcfd73
Kent C. Dodds <kent@doddsfamily.us>
2019-07-31 20:38:32 -0600

    install and run prettier

--- src/example.js
+++ src/example.js
@@ -1,10 +1,10 @@
-const username = 'freddy'
-typeof username === 'string'
+const username = "freddy";
+typeof username === "string";
 
-if (!('serviceWorker' in navigator)) {
+if (!("serviceWorker" in navigator)) {
   // you have an old browser :-(
 }
 
-const greeting = 'hello'
-console.log(`${greeting} world!`)
-;[1, 2, 3].forEach(x => console.log(x))
+const greeting = "hello";
+console.log(`${greeting} world!`);
+[1, 2, 3].forEach(x => console.log(x));
